d9c6b97f514f9b0307c6ef5f44cd3fdb0f427eca
William Macedo Sartini <williamsartinidev@outlook.com>
2024-02-07 16:18:38 -0300

    section main in home created

---
+++ app/views/home.php
@@ -0,0 +1,22 @@
+<div class="home">
+  <main>
+    <div class="perfil">
+      <div class="perfil-image">
+        <img src="/assets/images/bg-perfil.jpg" />
+      </div>
+        
+      <div class="perfil-title">
+        <h1>William Macedo Sartini</h1>
+        <h2> Desenvolvedor Web - Backend - PHP</h2>
+      </div>
+
+      <div class="perfil-skills">
+        <img src="/assets/icons/php-icon.png" />
+        <img src="/assets/icons/mysql-icon.png" />
+        <img src="/assets/icons/html-icon.png" />
+        <img src="/assets/icons/css-icon.png" />
+        <img src="/assets/icons/js-icon.png" />
+      </div>
+    </div>
+  </main>
+</div>
