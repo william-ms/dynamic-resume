d8581c8a72e61f8cfe7204c91487faf628f67ba8
William Macedo Sartini <williamsartinidev@outlook.com>
2024-02-09 19:24:57 -0300

    website icon added v1.2

--- app/views/main.php
+++ app/views/main.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="icon" href="<?php echo ICON_PATH ?>site-icon.png" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
     <script src="https://kit.fontawesome.com/360805e100.js" crossorigin="anonymous"></script>
     <link id="link" rel="stylesheet" href="<?php echo CSS_PATH; ?>index.css" />
   </head>
